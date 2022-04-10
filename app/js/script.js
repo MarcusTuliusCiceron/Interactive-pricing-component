942112abc5ffd19e312a7644177f9f6d726e6487
Ciceron <100491130+MarcusTuliusCiceron@users.noreply.github.com>
2022-04-10 18:46:22 +0200

    javascript working

--- app/js/script.js
+++ app/js/script.js
@@ -1,16 +1,49 @@
-const initProgress = 30
+const bar = document.querySelector('.input_bar')
+const pageview = document.querySelector('.pageview')
+const monthprice = document.querySelector('.monthprice')
+const billingToggle = document.querySelector('.billing_toggle')
 
-const thumb = document.querySelector('.thumb')
+const priceNodiscount = [8, 12, 16, 24, 36]
+const priceDiscount = [6, 9, 12, 18, 27] 
+
+const view = ["10K", "50K", "100K", "500K", "1M"]
+
+let index = 3
 
 
 
-thumb.addEventListener('mousedown', (e)=>{
+
+billingToggle.addEventListener("input", ()=>{
+    if (billingToggle.checked){
+        monthprice.innerText = `$${priceDiscount[index]} `
+    }
+    else{
+        monthprice.innerText = `$${priceNodiscount[index]} `
+    }
+
+})
+
+bar.addEventListener('input', ()=>{
+
+    if (bar.value >= priceNodiscount[index + 1]){
+        index = index + 1
+        
+    }
+    if (bar.value < priceNodiscount[index]){
+        index = index - 1
+    }
+    if (billingToggle.checked){
+        monthprice.innerText = `$${priceDiscount[index]} `
+    }
+    else{
+        monthprice.innerText = `$${priceNodiscount[index]} `
+    }
+    pageview.innerText = `${view[index]} pageviews`
     
+    console.log(index)
 })
 
 
 
+    //document.documentElement.style.setProperty('--progress', `90%` )
 
-function keyDown(e){
-    
-}
